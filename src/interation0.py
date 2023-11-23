87d6d061f0f66146802893ca0cf409a7236e1d64
pknw1 <pknw1@Desktop.local>
2023-11-23 00:38:12 +0000

    noobpy

--- src/interation0.py
+++ src/interation0.py
@@ -1,19 +1,46 @@
 import sqlite3
 #import re
 
-connection = sqlite3.connect("../testdata/ersatztv.sqlite3")
+connection = sqlite3.connect("../testdata/testdb.sqlite3")
 cursor = connection.cursor()
 
 
-rows = cursor.execute("SELECT id FROM PlexMovie ORDER BY id  ASC LIMIT 1").fetchall()
-for row in rows:
-    for item in row:
-        res = str(item) 
-        print(res)
-        title = cursor.execute("SELECT Title FROM MovieMetadata WHERE MovieId = 985").fetchall()
-        for movietitle in title:
-            displaytitle=str(movietitle)
-            test=movietitle[0]
-            print(test)
+plexmovies = cursor.execute("SELECT id FROM PlexMovie ORDER BY id  ASC LIMIT 1").fetchall()
+for movie_data in plexmovies:
+    for movie_id_raw in movie_data:
+        movie_id = str(movie_id_raw) 
+        print("processing movie_id: "+movie_id)
+        
+        title_raw = cursor.execute("SELECT Title FROM MovieMetadata WHERE MovieId = "+movie_id).fetchall()[0]
+        plot_raw = cursor.execute("SELECT Plot FROM MovieMetadata WHERE MovieId = "+movie_id).fetchall()[0]
+        year_raw = cursor.execute("SELECT year FROM MovieMetadata WHERE MovieId = "+movie_id).fetchall()[0]
+        title_year = title_raw[0]+" ("+str(year_raw[0])+")"
+
+        print(title_raw[0])
+        print(year_raw[0])
+        print(plot_raw[0])
+        print(title_year)
+        
+        # collection_id = Create a new collection with title_year
+        # Create a new collection_item with collection_id 
 
 
+      #  test = cursor.execute("SELECT seq FROM Sqlite_Sequence WHERE name = Library").fetchall()
+        #current_seq_id = cursor.execute("select seq from sqlite_sequence WHERE name = 'PlexMovie'").fetchall()[0]
+        #next_seq_id = current_seq_id[0]+1
+        #print(current_seq_id[0])
+        #print(next_seq_id)
+        #increment_seq_id = cursor.execute("select seq from sqlite_sequence WHERE name = 'PlexMovie'").fetchall()[0]
+        
+        collection_insert = cursor.execute("INSERT INTO Collection ('Name', 'UseCustomPlaybackOrder') VALUES ('"+title_year+"', 0)")
+        collection_seq_id = cursor.execute("select seq from sqlite_sequence WHERE name = 'Collection'").fetchall()[0]
+        next = str(collection_seq_id[0])
+        collection_item_insert = cursor.execute("INSERT INTO 'CollectionItem' ('CollectionId','MediaItemId','CustomIndex') VALUES ("+next+","+movie_id+",NULL);")
+        #filler_insert = cursor.execute()
+        #channel_insert = cursor.execute()
+
+        #INSERT INTO "FillerPreset" ("Id","Name","FillerKind","FillerMode","Duration","Count","PadToNearestMinute","CollectionType","CollectionId","MediaItemId","MultiCollectionId","SmartCollectionId","AllowWatermarks") VALUES (3,'Time Out',5,0,NULL,NULL,NULL,0,3,NULL,NULL,NULL,0);
+
+        connection.commit()
+        
+        
