55e97d6a6ecfda4b96d960c7d19455ee950d7458
pknw1 <pknw1@Desktop.local>
2023-11-24 20:59:39 +0000

    updated to include DB Writes - last main

--- src/interation0.py
+++ src/interation0.py
@@ -1,11 +1,15 @@
 import sqlite3
+import uuid
 #import re
 
 connection = sqlite3.connect("ersatztv.sqlite3")
 cursor = connection.cursor()
 
+#select * from Table_Name order by Column_Name offset 234 rows fetch next 16 rows only
+#plexmovies = cursor.execute("SELECT id FROM PlexMovie ORDER BY id  ASC LIMIT 6").fetchall()
 
-plexmovies = cursor.execute("SELECT id FROM PlexMovie ORDER BY id  ASC LIMIT 6").fetchall()
+
+plexmovies = cursor.execute("SELECT id FROM 'PlexMovie' ORDER BY id ASC").fetchall()
 for movie_data in plexmovies:
     for movie_id_raw in movie_data:
         movie_id = str(movie_id_raw) 
@@ -41,5 +45,13 @@ for movie_data in plexmovies:
         #filler_insert = cursor.execute()
         #channel_insert = cursor.execute()
 
-        filler_insert = cursor.execute("INSERT INTO FillerPreset ('Name','FillerKind','FillerMode','Duration','Count','PadToNearestMinute','CollectionType','CollectionId','MediaItemId','MultiCollectionId','SmartCollectionId','AllowWatermarks') VALUES ('"+title_year+"',5,0,NULL,NULL,NULL,0,"+next+",NULL,NULL,NULL,1);")
+        filler_insert = cursor.execute("INSERT INTO FillerPreset ('Name','FillerKind','FillerMode','Duration','Count','PadToNearestMinute','CollectionType','CollectionId','MediaItemId','MultiCollectionId','SmartCollectionId','AllowWatermarks') VALUES ('"+title_year+"',5,0,NULL,NULL,NULL,0,"+next+",NULL,NULL,NULL,0);")
+        filler_seq_id = cursor.execute("select seq from sqlite_sequence WHERE name = 'FillerPreset'").fetchall()[0]
+        next = str(filler_seq_id[0])
+
+        channel_seq_id = cursor.execute("select seq from sqlite_sequence WHERE name = 'Channel'").fetchall()[0]
+        next_channel = str(channel_seq_id[0]+5)
+        random_uuid = str(uuid.uuid4())
+        channel_insert = cursor.execute("INSERT INTO Channel ('FFmpegProfileId','FallbackFillerId','Name','Number','PreferredAudioLanguageCode','StreamingMode','UniqueId','WatermarkId','Categories','Group','PreferredSubtitleLanguageCode','SubtitleMode','MusicVideoCreditsMode','PreferredAudioTitle','MusicVideoCreditsTemplate') VALUES  (5,'"+next+"','"+title_year+"','"+next_channel+"','',5,'"+random_uuid+"',NULL,NULL,'Movies','',0,0,NULL,NULL);")
+                
         connection.commit()
