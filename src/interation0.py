982b1f138e81a62660acaa327af80f0f44a51807
pknw1 <pknw1@Desktop.local>
2023-11-24 18:02:15 +0000

    updated to include DB Writes

--- src/interation0.py
+++ src/interation0.py
@@ -1,11 +1,11 @@
 import sqlite3
 #import re
 
-connection = sqlite3.connect("../testdata/testdb.sqlite3")
+connection = sqlite3.connect("ersatztv.sqlite3")
 cursor = connection.cursor()
 
 
-plexmovies = cursor.execute("SELECT id FROM PlexMovie ORDER BY id  ASC LIMIT 1").fetchall()
+plexmovies = cursor.execute("SELECT id FROM PlexMovie ORDER BY id  ASC LIMIT 6").fetchall()
 for movie_data in plexmovies:
     for movie_id_raw in movie_data:
         movie_id = str(movie_id_raw) 
@@ -15,10 +15,12 @@ for movie_data in plexmovies:
         plot_raw = cursor.execute("SELECT Plot FROM MovieMetadata WHERE MovieId = "+movie_id).fetchall()[0]
         year_raw = cursor.execute("SELECT year FROM MovieMetadata WHERE MovieId = "+movie_id).fetchall()[0]
         title_year = title_raw[0]+" ("+str(year_raw[0])+")"
-
+        '''
         print(title_raw[0])
         print(year_raw[0])
         print(plot_raw[0])
+        '''
+
         print(title_year)
         
         # collection_id = Create a new collection with title_year
@@ -31,7 +33,7 @@ for movie_data in plexmovies:
         #print(current_seq_id[0])
         #print(next_seq_id)
         #increment_seq_id = cursor.execute("select seq from sqlite_sequence WHERE name = 'PlexMovie'").fetchall()[0]
-        
+      #  
         collection_insert = cursor.execute("INSERT INTO Collection ('Name', 'UseCustomPlaybackOrder') VALUES ('"+title_year+"', 0)")
         collection_seq_id = cursor.execute("select seq from sqlite_sequence WHERE name = 'Collection'").fetchall()[0]
         next = str(collection_seq_id[0])
@@ -39,8 +41,5 @@ for movie_data in plexmovies:
         #filler_insert = cursor.execute()
         #channel_insert = cursor.execute()
 
-        #INSERT INTO "FillerPreset" ("Id","Name","FillerKind","FillerMode","Duration","Count","PadToNearestMinute","CollectionType","CollectionId","MediaItemId","MultiCollectionId","SmartCollectionId","AllowWatermarks") VALUES (3,'Time Out',5,0,NULL,NULL,NULL,0,3,NULL,NULL,NULL,0);
-
+        filler_insert = cursor.execute("INSERT INTO FillerPreset ('Name','FillerKind','FillerMode','Duration','Count','PadToNearestMinute','CollectionType','CollectionId','MediaItemId','MultiCollectionId','SmartCollectionId','AllowWatermarks') VALUES ('"+title_year+"',5,0,NULL,NULL,NULL,0,"+next+",NULL,NULL,NULL,1);")
         connection.commit()
-        
-        
